a5d5927e534826e2a4d2182239aefb4152aba617
deepak-singh11 <deepaksinghrajput112002@gmail.com>
2025-06-18 02:22:07 +0530

    creating frontend dockerfile

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { PersistGate } from 'redux-persist/integration/react';
-import { store,persistor } from './redux/store.ts';
+import { store,persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import { StrictMode } from 'react';
-import App from './App.tsx';
+import App from './App';
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
